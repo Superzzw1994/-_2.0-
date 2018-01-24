60cb644205fa0f56af26bac091ec469a2eec7cd0
zzw1994529 <weaponxxx1994@163.com>
2018-01-24 19:47:27 +0800

    thirdadd 添加详细注释

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,51 +1,73 @@
 <template>
   <div class ="tab">
+    <!-- 左侧内容区开始 -->
     <div class="content">
+      <!-- logo图片 -->
       <div class="avatar">
         <img :src="seller.avatar" alt="" width="64" height="64">
       </div>
+      <!---->
       <div class="right-content">
+        <!-- 店铺名字 -->
         <div class="title">
           <span class="seller_logo"></span>
           <span class="seller_name">{{seller.name}}</span>
         </div>
+        <!---->
+        <!-- 店铺描述 -->
         <div class="description">
          {{seller.description}}/{{seller.deliveryTime}}分钟送达
         </div>
+        <!---->
+        <!-- 店铺活动 -->
        <div class="supports" v-if="seller.supports">
          <span :class="classMap[seller.supports[0].type]" class="icon"></span>
          <span class="supports_des">{{seller.supports[0].description}}</span>
        </div>
+       <!---->
       </div>
+      <!-- 右侧按钮 -->
       <div class="support_count" v-if="seller.supports" @click="changeDetail">
           <span class="count">{{seller.supports.length}}个</span>
        </div>
+       <!---->
     </div>
+    <!-- 左侧内容区结束 -->
+    <!-- 下方公告开始 -->
     <div class="bulletin" @click="changeDetail">
       <span class="bulletin_title"></span><span class="bulletin_text">{{seller.bulletin}}</span>
     </div>
+    <!-- 公告结束 -->
+    <!-- 背景图开始 -->
     <div class="background">
       <img :src="seller.avatar" alt="" width="100%" height="100%">
     </div>
+    <!--  -->
+    <!-- 弹出详情页开始 -->
     <div class="detail" v-show="detailShow"></div>
+    <!---->
   </div>
 </template>
 <script>
 export default {
   props:{
+    //接收父组件参数(数据) seller
     seller:{
       type:Object
     }
   },
   created () {
+    // 创建映射
     this.classMap = ['decrease','discount','special','invoice','guarantee']
   },
   data(){
     return{
+      // params: detailShow 控制detail弹出
       detailShow : false
     }
   },
   methods: {
+    //改变detailShow，控制detail展示
      changeDetail (){
        this.detailShow = !this.detailShow
      }
@@ -53,6 +75,7 @@ export default {
 }
 </script>
 <style scoped>
+/* 根元素样式 */
 .tab{
   font-family: 'PingFang SC', 'STHeitiSC-Light', 'Helvetica-Light', arial, sans-serif;
   position: relative;
@@ -60,11 +83,13 @@ export default {
   color:#fff;
   background: rgba(7,17,27,0.5);
 }
+/* 左部内容区 */
 .content{
   padding:24px 12px 18px 24px;
   font-size:0;
   position: relative;
 } 
+/* 左部内容区 logo样式 */
 .avatar{
   display: inline-block;
   vertical-align: top;
@@ -72,10 +97,29 @@ export default {
 .avatar img{
   border-radius: 2px;
 }
+/* 左部内容区 右侧内容样式 */
 .right-content{
   display: inline-block;
   margin-left:16px;
 }
+/* 商品名称 */
+.title{
+  font-size:16px;
+  color:rgb(255,255,255);
+  line-height: 18px;
+  font-weight: bold;
+  margin: 2px 0 8px 0;
+}
+.seller_logo{
+  vertical-align: top;
+  display: inline-block;
+  width:30px;
+  height:18px;
+  background-size:30px 18px;
+  background-image:url('imgs/brand@2x.png');
+  background-repeat:no-repeat; 
+}
+/* 商品描述 */
 .description{
   font-size:12px;
   color:rgb(255,255,255);
@@ -83,6 +127,7 @@ export default {
   font-weight: 100;
   margin-bottom: 10px;
 }
+/* 商品活动 */
 .supports{
   margin-bottom: 2px;
 }
@@ -101,37 +146,7 @@ export default {
   background-repeat: no-repeat;
   background-size: 12px 12px;
 }
-.title{
-  font-size:16px;
-  color:rgb(255,255,255);
-  line-height: 18px;
-  font-weight: bold;
-  margin: 2px 0 8px 0;
-}
-.seller_logo{
-  vertical-align: top;
-  display: inline-block;
-  width:30px;
-  height:18px;
-  background-size:30px 18px;
-  background-image:url('imgs/brand@2x.png');
-  background-repeat:no-repeat; 
-}
-.decrease{
-  background: url('imgs/decrease_1@2x.png');
-}
-.discount{
-  background: url('imgs/discount_1@2x.png');
-}
-.special{
-  background: url('imgs/special_1@2x.png');
-}
-.invoice{
-  background: url('imgs/invoice_1@2x.png');
-}
-.guarantee{
-  background: url('imgs/guarantee_1@2x.png');
-}
+/* 右侧按钮 */
 .support_count{
   position: absolute;
   right:12px;
@@ -147,6 +162,23 @@ export default {
 .count{
   font-size:10px;
 }
+/* 映射样式 classMap */
+.decrease{
+  background: url('imgs/decrease_1@2x.png');
+}
+.discount{
+  background: url('imgs/discount_1@2x.png');
+}
+.special{
+  background: url('imgs/special_1@2x.png');
+}
+.invoice{
+  background: url('imgs/invoice_1@2x.png');
+}
+.guarantee{
+  background: url('imgs/guarantee_1@2x.png');
+}
+/* 下方公告样式 */
 .bulletin{
    height:28px;
    line-height:28px;
@@ -172,6 +204,7 @@ export default {
   font-size:10px;
   margin:0 4px;
 }
+/* header背景模糊样式 */
 .background{
   position: absolute;
   top:0;
@@ -181,6 +214,7 @@ export default {
   z-index: -1;
   filter:blur(10px);
 }
+/* detail弹出详情页样式 */
 .detail{
   position: fixed;
   width:100%;
@@ -194,6 +228,7 @@ export default {
   background: rgba(7,17,27,0.8);
   /* filter: blur(10px); */
 }
+/* 媒体查询，不同dpr显示不同图片 */
 @media screen and(-webkit-min-device-pixel-ratioa:3){
   .seller_logo{
     background: url('imgs/brand@3x.png')
